dea0ed63020cea824ca6339600452562c0e32a3f
white rose <whiterose.people7050@gmail.com>
2023-03-12 17:41:44 +0530

    changes made to handleResponse function

--- dns.c
+++ dns.c
@@ -61,7 +61,7 @@ unsigned char recordType;
 char recordName[20];
 unsigned int OUT_QUERY_SIZE;
 char SERVER_IN_USE[40];
-
+int QUERY_STEPS;
 
 
 
@@ -72,9 +72,51 @@ void parseInput(char *DNS_QUERY_TEMPLATE, char *query);
 void mymemset(void *ptr, const int value, const int size);
 struct addrinfo* configureRemoteHost();
 void sendQuery(char* DNS_QUERY,struct addrinfo* DNSCONFIGPTR,char **query);
-void processResponse(char *DNS_RESPONSE,int RESPONSE_SIZE,char *DNS_QUERY);
+void processResponse(unsigned char *DNS_RESPONSE,int RESPONSE_SIZE);
+void hexToBin(const unsigned char* hex_stream,const int nSize,int* bin_stream);
 
 
+
+struct DNS_answer{
+    short int class;
+    short int type;
+    int TTL;
+    short int rSize;
+    int ip[5];
+        
+    // pointer in case has more than one response.
+    struct DNS_answer *Next;
+};
+
+
+
+typedef struct DNSresponse{
+    // response header flags.
+    short int isValidResponse;
+    short int OPCODE;
+    short int isAutherativeServer;
+    short int isTruncated;
+    short int isRecurssionAvailable;
+    short int isRecurssionDesired;
+    short int isResponseAuthencated;
+    short int isAuthRequired;
+    short int responseCode;
+
+
+    // counters.
+    short int QACOUNT;
+    short int ANSCOUNT;
+    short int NSCOUNT;
+    short int ARCOUNT;
+
+
+    // answer section
+    struct DNS_answer *answers;
+
+
+
+
+}DNSresponseStruct;
 
 
 
@@ -124,11 +166,18 @@ int CLIENT_INIT(int argc, char *argv[]){
     }
 
 
+    
 
 
     // PROGRAMM-EXIT-CODE = 0 == SUCCESS.
     return 0;
 }
+
+
+
+
+
+
 
 
 
@@ -200,6 +249,10 @@ void parseInput(char *DNS_QUERY_TEMPLATE, char *query){
     while (LabelStorage[InnerLoopCounter]){
         DNS_QUERY_TEMPLATE[QUERY_START_INDEX++] = LabelStorage[InnerLoopCounter++];
     }
+
+    // here...
+    // QUERY_STEPS =  STANDARD_HEADER_SIZE + QUERY_START_INDEX + 1;
+
 
     DNS_QUERY_TEMPLATE[QUERY_START_INDEX++] = 0; // Padding
     DNS_QUERY_TEMPLATE[QUERY_START_INDEX++] = 0;
@@ -329,7 +382,7 @@ void sendQuery(char* DNS_QUERY,struct addrinfo* DNSCONFIGPTR,char **query){
     // Buffers to hold hostname & service.
     char remoteHost[100] = {0};
     char remoteHostService[100] = {0};
-    char receveBuffer[RECEVE_BUFFER_SIZE];
+    unsigned char receveBuffer[RECEVE_BUFFER_SIZE];
     int bytesSent;
     int bytesReceived;
 
@@ -412,34 +465,237 @@ void sendQuery(char* DNS_QUERY,struct addrinfo* DNSCONFIGPTR,char **query){
 
     
     bytesReceived = recvfrom(clientSocket,&receveBuffer,RECEVE_BUFFER_SIZE,0,0,0);
-    printf("[*] :  Received %d Bytes...\n",bytesReceived);
+    printf("[*] :  Received %d Bytes...\n",bytesReceived-1);
 
-    processResponse(receveBuffer,bytesReceived,DNS_QUERY);
+
+
+    // Processing-response & showing up on cli.
+    processResponse(receveBuffer,bytesReceived);
 
     
-
-
-    
-
     // free-up memory DNSCONFIGPTR.
     freeaddrinfo(DNSCONFIGPTR);
+
 }
 
+int binToDec(int* bin_stream,const int nSize){
+    int decimal = 0;
+    int startExpo = 1;
 
-
-
-
-// TODO -: Complete this on Sunday othterwise you'r dead.
-void processResponse(char *DNS_RESPONSE,const int RESPONSE_SIZE,char *DNS_QUERY){
-
-    char QID[3];
-
-    QID[0] = DNS_RESPONSE[0];
-    QID[1] = DNS_RESPONSE[1];
-
-    int temp = 0;
-    printf("Response : \n");
-    while(temp < RESPONSE_SIZE){
-        printf("%X ",DNS_RESPONSE[temp++]);
+    for(int i=(nSize-1); i >=0; i--){
+        // printf("%d ",bin_stream[i]);
+        if(startExpo == 1){
+            decimal += (bin_stream[i]*startExpo);
+            startExpo++;
+        }else{
+             decimal += (bin_stream[i]*startExpo);
+             startExpo *= 2;
+        }
     }
+
+
+    return decimal;
 }
+
+
+void findNlist(struct DNS_answer* NewNode,struct DNS_answer** Head){
+    struct DNS_answer* Temp =  *Head;
+
+    if(Temp == NULL){
+        *Head = NewNode;
+        return;
+    }
+
+   
+
+    while(Temp->Next != NULL){
+        Temp =  Temp->Next;
+    }
+
+    Temp->Next = NewNode;
+    NewNode->Next = NULL;
+
+}
+
+
+
+
+void processResponse(unsigned char *DNS_RESPONSE,const int RESPONSE_SIZE){
+
+    
+
+    // validating response size.
+    if(RESPONSE_SIZE < 12){
+        fprintf(stderr,"ERR : Response is Broken\n");
+        exit(1);
+    }
+
+
+    // validating response flags.
+    char hex_stream[] = {DNS_RESPONSE[2],DNS_RESPONSE[3]};
+    int hex_stream_size = sizeof(hex_stream)/sizeof(hex_stream[0]);
+    int bin_stream_size =  (hex_stream_size*8)+1;
+    int bin_stream[bin_stream_size];
+    hexToBin(hex_stream,hex_stream_size,bin_stream);
+
+
+    // dns-response storage.
+    DNSresponseStruct DNS_RESPONSE_;
+    mymemset(&DNS_RESPONSE_,0,sizeof(DNS_RESPONSE_));
+
+
+
+    // response flags
+    DNS_RESPONSE_.isValidResponse = bin_stream[0];
+    
+    int temparr[] = {bin_stream[1],bin_stream[2],bin_stream[3],bin_stream[4]};
+    DNS_RESPONSE_.OPCODE =  binToDec(temparr,sizeof(temparr)/sizeof(temparr[0]));
+    
+    DNS_RESPONSE_.isAutherativeServer = bin_stream[5];
+    DNS_RESPONSE_.isTruncated = bin_stream[6];
+    DNS_RESPONSE_.isRecurssionAvailable = bin_stream[7];
+    DNS_RESPONSE_.isRecurssionDesired = bin_stream[8];
+    DNS_RESPONSE_.isResponseAuthencated = bin_stream[10];
+    DNS_RESPONSE_.isAuthRequired = bin_stream[11];
+
+    int temparr_[] = {bin_stream[12],bin_stream[13],bin_stream[14],bin_stream[15]};
+    DNS_RESPONSE_.responseCode = binToDec(temparr_,sizeof(temparr_)/sizeof(temparr_[0]));
+
+    
+    // counter.
+    DNS_RESPONSE_.QACOUNT = DNS_RESPONSE[4] + DNS_RESPONSE[5];
+    DNS_RESPONSE_.ANSCOUNT = DNS_RESPONSE[6] + DNS_RESPONSE[7];
+    DNS_RESPONSE_.NSCOUNT = DNS_RESPONSE[8] + DNS_RESPONSE[9];
+    DNS_RESPONSE_.ARCOUNT = DNS_RESPONSE[10] + DNS_RESPONSE[11];
+
+
+
+
+
+    QUERY_STEPS = 11;
+    // skipping over the query section and padding and type + class.
+    int currentCounter = (QUERY_STEPS + 11 + 2 + 4);
+    DNS_RESPONSE_.answers = NULL;
+    
+
+    for(int i=0; i<DNS_RESPONSE_.ANSCOUNT; i++){
+        struct DNS_answer* newNode = (struct DNS_answer*)malloc(sizeof(struct DNS_answer));
+        if(newNode == NULL){
+            fprintf(stderr,"Err : Memory allocation failed\n");
+            exit(1);
+        }
+
+        // linking new node to List.
+        findNlist(newNode,&DNS_RESPONSE_.answers);
+        
+        
+        // skipping pointer bytes...
+        currentCounter +=2;
+        newNode->type = DNS_RESPONSE[++currentCounter] + DNS_RESPONSE[++currentCounter];
+        newNode->class = DNS_RESPONSE[currentCounter] + DNS_RESPONSE[++currentCounter];
+        newNode->TTL += DNS_RESPONSE[++currentCounter];
+        newNode->TTL += DNS_RESPONSE[++currentCounter];
+        newNode->TTL += DNS_RESPONSE[++currentCounter];
+        newNode->TTL += DNS_RESPONSE[++currentCounter];
+        
+         printf("%d BC\n",currentCounter);
+        // currentCounter++;
+        //--- TODO : Upgrade code for ipv6 array size to hold more values...
+        int ipsize = DNS_RESPONSE[currentCounter]  + DNS_RESPONSE[++currentCounter];
+        printf("%x C\n",DNS_RESPONSE[37]);
+    
+
+        currentCounter++;
+
+        printf("%d %d\n",currentCounter,ipsize);
+        
+        // for(int i=0; i<ipsize; i++){
+        //     newNode->ip[i] = DNS_RESPONSE[currentCounter];
+        //     currentCounter++;
+        // }
+
+        
+        currentCounter++;
+    }
+
+
+
+        for(int i=0; i<4; i++){
+            printf("%d.",DNS_RESPONSE_.answers  ->ip[i]);
+        }
+    // printf("%d\n",DNS_RESPONSE_.ANSCOUNT);
+    // printf("%d.",DNS_RESPONSE_.ANSCOUNT);
+    
+
+
+
+}
+
+
+
+
+
+
+ // convert hex_stream to stream of binery...
+void hexToBin(const unsigned char* hex_stream,const int nSize,int* bin_stream){
+
+    int binarrsize =  (nSize*8)+1;
+    mymemset(bin_stream,0,binarrsize);
+    
+    int tempbin[8] = {0};
+    int innreloopcounter = 7;
+    int bin_arr_counter = 0;
+
+    // outerloop for each hex_degit
+    for(int i=0; i<nSize; i++){
+        int currentHex = hex_stream[i];
+        while(currentHex != 0){
+            tempbin[innreloopcounter] = currentHex%2;
+            currentHex = currentHex/2;
+            innreloopcounter--;
+        }
+        for(int i=0; i<8; i++){
+            bin_stream[bin_arr_counter] = tempbin[i];
+            bin_arr_counter++;
+        }
+
+        mymemset(tempbin,0,8);
+        innreloopcounter = 7;
+    }
+
+    return;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+        
+
